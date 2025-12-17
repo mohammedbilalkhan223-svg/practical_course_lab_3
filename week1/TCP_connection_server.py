f1beb184188b6ec16b2d3e47225431b2ae84cac1
unknown <khan.bilal619@gmail.com>
2025-12-17 15:34:15 +0100

    No changes

--- week1/TCP_connection_server.py
+++ week1/TCP_connection_server.py
@@ -5,7 +5,7 @@ TCP_PORT = 5005
 BUFFER_SIZE = 500
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1) #allows to reuse the socket
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #allows to reuse the socket
 
 
 s.bind((TCP_IP, TCP_PORT))
